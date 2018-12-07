86d05d863d1ec104c941fc02b610bfcfc4e12982
bonnina <37539513+bonnina@users.noreply.github.com>
2018-12-07 18:38:40 +0200

    Create profile function (for protected route)

---
+++ auth/profileHandler.js
@@ -0,0 +1,35 @@
+const connectToDatabase = require('../DB');
+const User = require('../user/model');
+
+/* function */
+
+module.exports.profile = (event, context) => {
+  console.log(event.requestContext.authorizer);
+  
+  context.callbackWaitsForEmptyEventLoop = false;
+  return connectToDatabase()
+    .then(() =>
+      findUser(event.requestContext.authorizer.principalId) 
+    )
+    .then(session => ({
+      statusCode: 200,
+      body: JSON.stringify(session)
+    }))
+    .catch(err => ({
+      statusCode: err.statusCode || 500,
+      headers: { 'Content-Type': 'text/plain' },
+      body: { stack: err.stack, message: err.message }
+    }));
+};
+
+/* helper */
+
+function findUser(name) {
+  return User.findOne({ username: name })
+    .then(user =>
+      !user
+        ? Promise.reject('No user found.')
+        : user
+    )
+    .catch(err => Promise.reject(new Error(err)));
+}
